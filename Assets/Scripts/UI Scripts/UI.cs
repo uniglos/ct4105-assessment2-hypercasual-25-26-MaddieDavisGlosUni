0d970e74b077a10f7c39941cdbf9fa24de09fb87
MaddieDavisGlosUni <s4511561@glos.ac.uk>
2026-03-19 14:15:36 +0000

    Fised UI play button
    Fixed the button

---
+++ Assets/Scripts/UI Scripts/UI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+
+     public void SceneLoader()
+    {
+        SceneManager.LoadScene("Shoot Variation");
+        Debug.Log("Pressed");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
