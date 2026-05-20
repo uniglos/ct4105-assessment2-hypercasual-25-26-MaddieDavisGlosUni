c77bf826a9edb51d70b6ae324963bf06c7448519
RonnieFletcherGlosUni <s4515249@glos.ac.uk>
2026-05-20 09:44:30 +0100

    Timer Script
    From youtube tutorial

---
+++ Assets/Timer_script.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer_script : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        
+    }
+}
